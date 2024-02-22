f0834937c688a94c6b6e1f62f8137f1c191707a4
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-22 18:20:37 +0100

    Start small app: "formapp" by creating model of User

---
+++ src/main/java/io/github/bartlomiejmilosz/servlets/formapp/model/User.java
@@ -0,0 +1,44 @@
+package io.github.bartlomiejmilosz.servlets.formapp.model;
+
+public class User {
+    private String username;
+    private String password;
+    private String gender;
+    private String[] hobbies;
+
+    // Konstruktory, gettery i settery
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+}
