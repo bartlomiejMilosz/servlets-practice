069a74c2c553dcd0216825577e126839a1b5e834
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-22 18:21:47 +0100

    Add servlet for saving form.html

---
+++ src/main/java/io/github/bartlomiejmilosz/servlets/formapp/controllers/FormServlet.java
@@ -0,0 +1,17 @@
+package io.github.bartlomiejmilosz.servlets.formapp.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/form")
+public class FormServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/formappresources/form.html").forward(req, resp);
+    }
+}
